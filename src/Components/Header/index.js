ec090c62a56b44fc71ad7ac509331a743c01b0e4
limserhane <limserhane@protonmail.com>
2021-11-08 12:48:39 +0100

    feat: implement base header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style.css";
+
+function Header(props) {
+	return (
+		<div className="header-container">
+			<h1 className="title">Care And Park</h1>
+			<a className="link">Parkings</a>
+		</div>
+	);
+}
+
+export default Header;
