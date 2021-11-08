c09af5e5b1d3265792cc3637bae1cc0797732514
limserhane <limserhane@protonmail.com>
2021-11-08 12:42:24 +0100

    fix: initialize

---
+++ src/Components/App/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Care And Park</h1>    
+    </div>
+  );
+}
+
+export default App;
