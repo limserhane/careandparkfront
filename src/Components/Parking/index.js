7bd4216c36fdfd2e6ca2043f73bbfdac70ae7237
limserhane <limserhane@protonmail.com>
2021-11-08 13:00:52 +0100

    feat: implement base parking

---
+++ src/Components/Parking/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./style.css";
+
+function Parking(props) {
+	return (
+        <div className="parking-container">
+            <h2>Parking : ?</h2>
+            <p>Adresse : ?</p>
+        </div>
+	);
+}
+
+export default Parking;
