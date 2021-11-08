42f5b1efd641b364094db57e736085525f1dc91e
limserhane <limserhane@protonmail.com>
2021-11-08 14:25:18 +0100

    feat: implement header title and links

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,11 +2,16 @@ import React from "react";
 
 import "./style.css";
 
+import * as api from "../../Utils/api"
+
+import {NavLink} from "react-router-dom"
+
 function Header(props) {
 	return (
 		<div className="header-container">
-			<h1 className="title">Care And Park</h1>
-			<p className="link">Parkings</p>
+			<NavLink to="/" className="link"><h1 className="title">Care And Park</h1></NavLink>
+			<NavLink to="/parkings" className="sublink link">Parkings</NavLink>
+			<a href={api.url} target="_blank" className="sublink link">API</a>
 		</div>
 	);
 }
