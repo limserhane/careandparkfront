933d9a0f6b5574ab28beb99b1dd13b654f73cb75
limserhane <limserhane@protonmail.com>
2021-11-08 13:56:55 +0100

    feat: implement routes and app outlet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './style.css';
+
 import App from './Components/App';
+import Parkings from "./Components/Parkings"
+import Parking from "./Components/Parking"
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<App />} >
+				{/* NO INDEX = blank home */}
+				<Route path="parkings" element={<Parkings/>} />
+				<Route path="parkings/:id" element={<Parking />} />
+			</Route>
+		</Routes>
+	</BrowserRouter>
+</React.StrictMode>,
+document.getElementById('root')
 );
