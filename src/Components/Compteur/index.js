85dcd7a6233483bd66b7c4bae1484319ebb37a31
limserhane <limserhane@protonmail.com>
2021-11-08 20:23:24 +0100

    feat: implementation compteur operations

--- src/Components/Compteur/index.js
+++ src/Components/Compteur/index.js
@@ -4,24 +4,46 @@ import "./style.css";
 
 import * as api from "../../Utils/api"
 
-import {useParams} from "react-router-dom"
+
+function changementCompteur(parkingId, value, set) {
+    
+    const request = new Request(api.url+"/parkings/"+parkingId+"/compteur/"+value, {
+        method: "POST"
+    })
+
+    fetch(request)
+    .then( (response) => {
+        if(response.ok) { response.json().then( (data) => {
+            set(data)
+        })}
+        else {
+            if(response.status === 409) {
+                alert("Impossible d'enregistrer une sortie")
+            }
+        }
+    })
+    .catch( (error) => {
+        console.log(error)
+    })
+
+}
 
 function Compteur(props) {
 
-    const {id} = useParams();
+    const id = props.id
 
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
 
     const [compteur, setCompteur] = useState(0)
 
-    useEffect( () => {
+    function chargerCompteur() {
     
         fetch(api.url+"/parkings/"+id+"/compteur")
         .then( (response) => {
             if(response.ok) { response.json().then( (data) => {
                 setCompteur(data)
-                    setLoading(false);
+                setLoading(false);
             })}
             else {
                 // reponse not ok
@@ -31,7 +53,12 @@ function Compteur(props) {
             setError(error);
             setLoading(false);
         })
-    })
+    }
+
+    useEffect( () => {
+        chargerCompteur()
+    }, [])
+
 
     if(loading) {
         return <span>Chargement du compteur... {id} ...</span>
@@ -48,9 +75,10 @@ function Compteur(props) {
 
 	return (
         <div className="compteur-container">
-            <p className="nom">Nombre de véhicules : {compteur}</p>
-            <button>Sortie</button>
-            <button>Entrée</button>
+            <p className="compteur">Nombre de véhicules : {compteur}</p>
+            <button className="bouton recharger" onClick={() => chargerCompteur()}>Recharger</button>
+            <button className="bouton sortie" onClick={() => changementCompteur(id, "sortie", setCompteur)}>Sortie</button>
+            <button className="bouton entree" onClick={() => changementCompteur(id, "entree", setCompteur)}>Entrée</button>
         </div>
 	);
 }
--- src/Components/Parking/index.js
+++ src/Components/Parking/index.js
@@ -5,11 +5,15 @@ import "./style.css";
 import Compteur from "../Compteur";
 import ParkingInformations from "../ParkingInformations";
 
+import { useParams } from "react-router-dom";
+
 function Parking(props) {
+    const {id} = useParams();
+
 	return (
         <div className="parking-container">
-            <ParkingInformations />
-            <Compteur />
+            <ParkingInformations id={id}/>
+            <Compteur id={id}/>
         </div>
 	);
 }
