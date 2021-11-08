be5b142472f8f205329ca85672d503f4d3c06802
limserhane <limserhane@protonmail.com>
2021-11-08 19:11:38 +0100

    feat: implement parking list fetching

--- src/Components/Parkings/index.js
+++ src/Components/Parkings/index.js
@@ -1,35 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./style.css";
 
+import * as api from "../../Utils/api"
+
 import {NavLink} from "react-router-dom";
 
-function obtenirParkings() {
-    return [
-        {
-            id: "1",
-            nom: "Parking de la mairie",
-            adresse: "Mairie"
-        },
-        {
-            id: "2",
-            nom: "Parking du gymnase",
-            adresse: "Gymnase"
-        },
-        {
-            id: "3",
-            nom: "Parking du centre-ville",
-            adresse: "Centre-ville"
-        }
-    ];
-}
-
 function Parkings(props) {
+    
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const [parkings, setParkings] = useState([])
+
+    useEffect(() => {
+
+        setLoading(true);
+    
+        fetch(api.url+"/parkings")
+        .then( (response) => {
+            if(response.ok) { response.json().then( (data) => {
+                    setParkings(data)
+                    setLoading(false);
+            })}
+            else {
+                // reponse not ok
+            }
+        })
+        .catch( (error) => {
+            setError(error);
+            setLoading(false);
+        })
+    }, [])
+
+    if(loading) {
+        return <span>Chargement des parkings ...</span>
+    }
+
+    if(error) {
+        return (
+            <div>
+                <h2>Erreur lors du chargement des parkings</h2>
+                <p>{JSON.stringify(error)}</p>
+            </div>
+        );
+    }
+
 	return (
 		<ul className="parkings-container">
-			{obtenirParkings().map(parking => 
-                <li className="parking">
-                    <NavLink to={parking.id} className="link">{parking.nom}, {parking.adresse}</NavLink>
+			{parkings.map(parking => 
+                <li key={parking.id} className="parking">
+                    <NavLink to={parking.id.toString()} className="link">{parking.nom}, {parking.adresse}</NavLink>
                 </li>
             )}
 		</ul>
