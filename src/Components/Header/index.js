c9c07202b9f6d1a8104b0177645ea5dda65fb7eb
Serhane LIM <dev@limserhane.fr>
2021-11-09 16:52:53 +0100

    feat: add nouvelle place button nad behaviour

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,15 +4,19 @@ import "./style.css";
 
 import * as api from "../../Utils/api"
 
-import {NavLink} from "react-router-dom"
+import {NavLink, useParams} from "react-router-dom"
 
 function Header(props) {
+
+	let {id} = useParams();
+
 	return (
 		<div className="header-container">
 			<NavLink to="/" className="link"><h1 className="title">Care And Park</h1></NavLink>
 			<NavLink to="/parkings" className="sublink link">Parkings</NavLink>
 			<a href={api.url} target="_blank" rel="noreferrer" className="sublink link">API</a>
 			<NavLink to="/parkings/new" className="sublink link">Nouveau parking</NavLink>
+			{id && <NavLink to={"/parkings/"+id+"/places/new"} className="sublink link">Nouvelle place</NavLink>}
 		</div>
 	);
 }
--- src/Components/NouvellePlace/index.js
+++ src/Components/NouvellePlace/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 import * as api from "../../Utils/api"
 
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate} from "react-router-dom";
 
 function NouvellePlace (props) {
 
@@ -13,7 +13,7 @@ function NouvellePlace (props) {
     const [numero, setNumero] = useState(0);
     const [type, setType] = useState("");
 
-    // const [fichier, setFichier] = useState("");
+    let navigate = useNavigate();
 
     function envoyer(event) {
         if(event) event.preventDefault();
@@ -93,6 +93,8 @@ function NouvellePlace (props) {
             })
 
         });
+
+        // navigate("/parkings/"+id, {replace:true})
     }
 
     return (
@@ -118,7 +120,6 @@ function NouvellePlace (props) {
                 <label>Fichier JSON à charger
                     <input type="file" onChange={(event)=>{chargerFichier(event)}}/>
                 </label>
-                {/* <input type="submit" value="Créer les places" /> */}
             </form>
 
         </div>
